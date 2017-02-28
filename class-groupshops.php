1eebab089c13eb6b248462063a3c412d5b94aded
thanhnguyenthi086@gmail.com <thanhnguyenthi086@gmail.com>
2017-02-28 21:49:43 +0700

    đổi tên class-groupshops.php

--- class-groupshops.php
+++ class-groupshops.php
@@ -21,18 +21,18 @@
 /**
  *   Plugin activation hook 
  */
-function wcvendors_activate() {
+function topgroupshops_activate() {
 
 	/**
 	 *  Requires woocommerce to be installed and active 
 	 */
 	if ( !class_exists( 'WooCommerce' ) ) { 
 		deactivate_plugins( plugin_basename( __FILE__ ) );
-		wp_die( __( 'WC Vendors requires WooCommerce to run. Please install WooCommerce and activate before attempting to activate again.', 'wcvendors' ) );
+		wp_die( __( 'Group Shops requires WooCommerce to run. Please install WooCommerce and activate before attempting to activate again.', 'topgroupshops' ) );
 	}
-} // wcvendors_activate()
+} // topgroupshops_activate()
 
-register_activation_hook( __FILE__, 'wcvendors_activate' );
+register_activation_hook( __FILE__, 'topgroupshops_activate' );
 
 
 /**
@@ -43,23 +43,23 @@ require_once trailingslashit( dirname( __FILE__ ) ) . 'classes/includes/class-fu
 /**
  * Check if WooCommerce is active
  */
-if ( wcv_is_woocommerce_activated() ) {
+if ( tgs_is_woocommerce_activated() ) {
 
 	/* Define an absolute path to our plugin directory. */
-	if ( !defined( 'wcv_plugin_dir' ) ) 		define( 'wcv_plugin_dir', trailingslashit( dirname( __FILE__ ) ) . '/' );
-	if ( !defined( 'wcv_assets_url' ) ) 		define( 'wcv_assets_url', trailingslashit( plugins_url( 'assets', __FILE__ ) ) );
-	if ( !defined( 'wcv_plugin_base' ) ) 		define( 'wcv_plugin_base', plugin_basename( __FILE__ ) );
-	if ( !defined( 'wcv_plugin_dir_path' ) )	define( 'wcv_plugin_dir_path', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
+	if ( !defined( 'tgs_plugin_dir' ) ) 		define( 'tgs_plugin_dir', trailingslashit( dirname( __FILE__ ) ) . '/' );
+	if ( !defined( 'tgs_assets_url' ) ) 		define( 'tgs_assets_url', trailingslashit( plugins_url( 'assets', __FILE__ ) ) );
+	if ( !defined( 'tgs_plugin_base' ) ) 		define( 'tgs_plugin_base', plugin_basename( __FILE__ ) );
+	if ( !defined( 'tgs_plugin_dir_path' ) )	define( 'tgs_plugin_dir_path', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 
 
-	define('WCV_VERSION', '1.9.7' ); 
+	define('TGS_VERSION', '1.9.7' ); 
 
 	/**
 	 * Main Product Vendor class
 	 *
-	 * @package WCVendors
+	 * @package TGSVendors
 	 */
-	class WC_Vendors
+	class TGS_Vendors
 	{
 
 		/**
@@ -77,12 +77,12 @@ if ( wcv_is_woocommerce_activated() ) {
 			// Load text domain 
 			add_action( 'plugins_loaded', array( $this, 'load_il8n' ) );
 
-			$this->title = __( 'WC Vendors', 'wcvendors' );
+			$this->title = __( 'Group Shops', 'topgroupshops' );
 
 			// Install & upgrade
 			add_action( 'admin_init', array( $this, 'check_install' ) );
 			add_action( 'admin_init', array( $this, 'maybe_flush_permalinks' ), 99 );
-			add_action( 'admin_init', array( $this, 'wcv_required_ignore_notices' ) );
+			add_action( 'admin_init', array( $this, 'tgs_required_ignore_notices' ) );
 
 			add_action( 'plugins_loaded', array( $this, 'load_settings' ) );
 			add_action( 'plugins_loaded', array( $this, 'include_gateways' ) );
@@ -105,7 +105,7 @@ if ( wcv_is_woocommerce_activated() ) {
 		 */
 		public function invalid_wc_version()
 		{
-			echo '<div class="error"><p>' . __( '<b>WC Vendors is disabled</b>. WC Vendors requires a minimum of WooCommerce v2.5.0.', 'wcvendors' ) . '</p></div>';
+			echo '<div class="error"><p>' . __( '<b>Group Shops is disabled</b>. Group Shops requires a minimum of WooCommerce v2.5.0.', 'topgroupshops' ) . '</p></div>';
 		}
 
 		/**
@@ -146,10 +146,10 @@ if ( wcv_is_woocommerce_activated() ) {
 				return false;
 			}
 
-			require_once wcv_plugin_dir . 'classes/class-install.php';
+			require_once tgs_plugin_dir . 'classes/class-install.php';
 
 			$this->load_settings();
-			$install = new WCV_Install;
+			$install = new TGS_Install;
 			$install->init();
 		}
 
@@ -160,22 +160,22 @@ if ( wcv_is_woocommerce_activated() ) {
 		public function load_settings()
 		{
 			if ( empty( self::$pv_options ) ) {
-				require_once wcv_plugin_dir . 'classes/admin/settings/classes/sf-class-settings.php';
+				require_once tgs_plugin_dir . 'classes/admin/settings/classes/sf-class-settings.php';
 				self::$pv_options = new SF_Settings_API( self::$id, $this->title, 'woocommerce', __FILE__ );
-				self::$pv_options->load_options( wcv_plugin_dir . 'classes/admin/settings/sf-options.php' );
+				self::$pv_options->load_options( tgs_plugin_dir . 'classes/admin/settings/sf-options.php' );
 			}
 		}
 
 		public function load_il8n() { 
 
-			$domain = 'wcvendors';
+			$domain = 'topgroupshops';
 		    
 		    $locale = apply_filters( 'plugin_locale', get_locale(), $domain );
 
 		    //Place your custom translations into wp-content/languages/wc-vendors to be upgrade safe 
 		    load_textdomain($domain, WP_LANG_DIR.'/wc-vendors/'.$domain.'-'.$locale.'.mo');
 			
-			load_plugin_textdomain( 'wcvendors', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+			load_plugin_textdomain( 'topgroupshops', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 
 		}
 
@@ -185,48 +185,48 @@ if ( wcv_is_woocommerce_activated() ) {
 		 */
 		public function include_core()
 		{
-			require_once wcv_plugin_dir . 'classes/class-queries.php';
-			require_once wcv_plugin_dir . 'classes/class-vendors.php';
-			require_once wcv_plugin_dir . 'classes/class-cron.php';
-			require_once wcv_plugin_dir . 'classes/class-commission.php';
-			require_once wcv_plugin_dir . 'classes/class-shipping.php';
-			require_once wcv_plugin_dir . 'classes/class-vendor-order.php';
-			require_once wcv_plugin_dir . 'classes/class-vendor-post-types.php'; 
-			require_once wcv_plugin_dir . 'classes/front/class-vendor-cart.php';
-			require_once wcv_plugin_dir . 'classes/front/dashboard/class-vendor-dashboard.php';
-			require_once wcv_plugin_dir . 'classes/front/class-vendor-shop.php';
-			require_once wcv_plugin_dir . 'classes/front/signup/class-vendor-signup.php';
-			require_once wcv_plugin_dir . 'classes/front/orders/class-orders.php';
-			require_once wcv_plugin_dir . 'classes/admin/emails/class-emails.php';
-			require_once wcv_plugin_dir . 'classes/admin/class-product-meta.php';
-			require_once wcv_plugin_dir . 'classes/admin/class-vendor-applicants.php';
-			require_once wcv_plugin_dir . 'classes/admin/class-vendor-reports.php';
-			require_once wcv_plugin_dir . 'classes/admin/class-admin-reports.php';
-			require_once wcv_plugin_dir . 'classes/admin/class-admin-users.php';
-			require_once wcv_plugin_dir . 'classes/admin/class-admin-page.php';
-			require_once wcv_plugin_dir . 'classes/admin/class-vendor-admin-dashboard.php'; 
-			require_once wcv_plugin_dir . 'classes/includes/class-wcv-shortcodes.php';
+			require_once tgs_plugin_dir . 'classes/class-queries.php';
+			require_once tgs_plugin_dir . 'classes/class-vendors.php';
+			require_once tgs_plugin_dir . 'classes/class-cron.php';
+			require_once tgs_plugin_dir . 'classes/class-commission.php';
+			require_once tgs_plugin_dir . 'classes/class-shipping.php';
+			require_once tgs_plugin_dir . 'classes/class-vendor-order.php';
+			require_once tgs_plugin_dir . 'classes/class-vendor-post-types.php'; 
+			require_once tgs_plugin_dir . 'classes/front/class-vendor-cart.php';
+			require_once tgs_plugin_dir . 'classes/front/dashboard/class-vendor-dashboard.php';
+			require_once tgs_plugin_dir . 'classes/front/class-vendor-shop.php';
+			require_once tgs_plugin_dir . 'classes/front/signup/class-vendor-signup.php';
+			require_once tgs_plugin_dir . 'classes/front/orders/class-orders.php';
+			require_once tgs_plugin_dir . 'classes/admin/emails/class-emails.php';
+			require_once tgs_plugin_dir . 'classes/admin/class-product-meta.php';
+			require_once tgs_plugin_dir . 'classes/admin/class-vendor-applicants.php';
+			require_once tgs_plugin_dir . 'classes/admin/class-vendor-reports.php';
+			require_once tgs_plugin_dir . 'classes/admin/class-admin-reports.php';
+			require_once tgs_plugin_dir . 'classes/admin/class-admin-users.php';
+			require_once tgs_plugin_dir . 'classes/admin/class-admin-page.php';
+			require_once tgs_plugin_dir . 'classes/admin/class-vendor-admin-dashboard.php'; 
+			require_once tgs_plugin_dir . 'classes/includes/class-tgs-shortcodes.php';
 
 
 			if ( !function_exists( 'woocommerce_wp_text_input' ) && !is_admin() ) {
 				include_once(WC()->plugin_path() . '/includes/admin/wc-meta-box-functions.php');
 			}
 
-			new WCV_Vendors;
-			new WCV_Vendor_Shop;
-			new WCV_Vendor_Cart;
-			new WCV_Commission;
-			new WCV_Shipping;
-			new WCV_Cron;
-			new WCV_Orders;
-			new WCV_Vendor_Dashboard;
-			new WCV_Admin_Setup;
-			new WCV_Vendor_Admin_Dashboard; 
-			new WCV_Admin_Reports;
-			new WCV_Vendor_Applicants;
-			new WCV_Emails;
-			new WCV_Vendor_Signup;
-			new WCV_Shortcodes; 
+			new TGS_Vendors;
+			new TGS_Vendor_Shop;
+			new TGS_Vendor_Cart;
+			new TGS_Commission;
+			new TGS_Shipping;
+			new TGS_Cron;
+			new TGS_Orders;
+			new TGS_Vendor_Dashboard;
+			new TGS_Admin_Setup;
+			new TGS_Vendor_Admin_Dashboard; 
+			new TGS_Admin_Reports;
+			new TGS_Vendor_Applicants;
+			new TGS_Emails;
+			new TGS_Vendor_Signup;
+			new TGS_Shortcodes; 
 		}
 
 
@@ -238,9 +238,9 @@ if ( wcv_is_woocommerce_activated() ) {
 		 */
 		public function include_init(){ 
 
-			new WCV_Vendor_Reports;
-			new WCV_Product_Meta;
-			new WCV_Admin_Users;
+			new TGS_Vendor_Reports;
+			new TGS_Product_Meta;
+			new TGS_Admin_Users;
 
 		} // include_init() 
 
@@ -252,7 +252,7 @@ if ( wcv_is_woocommerce_activated() ) {
 			$screen = get_current_screen(); 
 
 			if ( in_array( $screen->id, array( 'edit-product' ) ) ) {
-				wp_enqueue_script( 'wcv_quick-edit', wcv_assets_url. 'js/wcv-admin-quick-edit.js', array('jquery') );
+				wp_enqueue_script( 'tgs_quick-edit', tgs_assets_url. 'js/tgs-admin-quick-edit.js', array('jquery') );
 			}
 
 		}
@@ -263,9 +263,9 @@ if ( wcv_is_woocommerce_activated() ) {
 		 */
 		public function include_gateways()
 		{
-			require_once wcv_plugin_dir . 'classes/gateways/PayPal_AdvPayments/paypal_ap.php';
-			require_once wcv_plugin_dir . 'classes/gateways/PayPal_Masspay/class-paypal-masspay.php';
-			require_once wcv_plugin_dir . 'classes/gateways/WCV_Gateway_Test/class-wcv-gateway-test.php';
+			require_once tgs_plugin_dir . 'classes/gateways/PayPal_AdvPayments/paypal_ap.php';
+			require_once tgs_plugin_dir . 'classes/gateways/PayPal_Masspay/class-paypal-masspay.php';
+			require_once tgs_plugin_dir . 'classes/gateways/TGS_Gateway_Test/class-tgs-gateway-test.php';
 		}
 
 
@@ -278,7 +278,7 @@ if ( wcv_is_woocommerce_activated() ) {
 		public function option_updates( $options, $tabname )
 		{
 			// Change the vendor role capabilities
-			if ( $tabname == sanitize_title(__( 'Capabilities', 'wcvendors' )) ) {
+			if ( $tabname == sanitize_title(__( 'Capabilities', 'topgroupshops' )) ) {
 				$can_add          = $options[ 'can_submit_products' ];
 				$can_edit         = $options[ 'can_edit_published_products' ];
 				$can_submit_live  = $options[ 'can_submit_live_products' ];
@@ -301,18 +301,18 @@ if ( wcv_is_woocommerce_activated() ) {
 
 				remove_role( 'vendor' );
 				
-				add_role( 'vendor', __('Vendor', 'wcvendors'), $args );
+				add_role( 'vendor', __('Vendor', 'topgroupshops'), $args );
 			} // Update permalinks
-			else if ( $tabname == sanitize_title(__( 'General', 'wcvendors' ) )) {
-				$old_permalink = WC_Vendors::$pv_options->get_option( 'vendor_shop_permalink' );
+			else if ( $tabname == sanitize_title(__( 'General', 'topgroupshops' ) )) {
+				$old_permalink = TGS_Vendors::$pv_options->get_option( 'vendor_shop_permalink' );
 				$new_permalink = $options[ 'vendor_shop_permalink' ];
 
 				if ( $old_permalink != $new_permalink ) {
-					update_option( WC_Vendors::$id . '_flush_rules', true );
+					update_option( TGS_Vendors::$id . '_flush_rules', true );
 				}
 			}
 
-			do_action( 'wcvendors_option_updates', $options, $tabname ); 
+			do_action( 'topgroupshops_option_updates', $options, $tabname ); 
 
 		}
 
@@ -324,9 +324,9 @@ if ( wcv_is_woocommerce_activated() ) {
 		*/
 		public function maybe_flush_permalinks()
 		{
-			if ( get_option( WC_Vendors::$id . '_flush_rules' ) ) {
+			if ( get_option( TGS_Vendors::$id . '_flush_rules' ) ) {
 				flush_rewrite_rules();
-				update_option( WC_Vendors::$id . '_flush_rules', false );
+				update_option( TGS_Vendors::$id . '_flush_rules', false );
 			}
 		}
 
@@ -338,12 +338,12 @@ if ( wcv_is_woocommerce_activated() ) {
 		 * 
 		 */
 		public static function plugin_row_meta( $links, $file ) {
-			if ( $file == wcv_plugin_base ) {
+			if ( $file == tgs_plugin_base ) {
 
 				$row_meta = array(
-	                            'docs' 		=> '<a href="http://www.wcvendors.com/kb/" target="_blank">'.__( 'Documentation/KB', 'wcvendors' ).'</a>',
-	                            'help' 		=> '<a href="http://www.wcvendors.com/help/" target="_blank">'.__( 'Help Forums', 'wcvendors').'</a>',
-	                            'support' 	=> '<a href="http://www.wcvendors.com/contact-us/" target="_blank">'.__( 'Paid Support', 'wcvendors' ).'</a>'
+	                            'docs' 		=> '<a href="http://www.topgroupshops.com/kb/" target="_blank">'.__( 'Documentation/KB', 'topgroupshops' ).'</a>',
+	                            'help' 		=> '<a href="http://www.topgroupshops.com/help/" target="_blank">'.__( 'Help Forums', 'topgroupshops').'</a>',
+	                            'support' 	=> '<a href="http://www.topgroupshops.com/contact-us/" target="_blank">'.__( 'Paid Support', 'topgroupshops' ).'</a>'
 	                        );
 
 				return array_merge( $links, $row_meta );
@@ -357,16 +357,16 @@ if ( wcv_is_woocommerce_activated() ) {
 		 * @access public
          * 
 		 */
-		public function wcv_required_ignore_notices(){
+		public function tgs_required_ignore_notices(){
 			global $current_user;
     		$current_user_id = $current_user->ID;
     		
 	        /* If user clicks to ignore the notice, add that to their user meta */
-	        if ( isset( $_GET[ 'wcv_shop_ignore_notice' ] ) && '0' == $_GET[ 'wcv_shop_ignore_notice' ] ) {
-	            add_user_meta( $current_user_id, 'wcv_shop_ignore_notice', 'true', true);
+	        if ( isset( $_GET[ 'tgs_shop_ignore_notice' ] ) && '0' == $_GET[ 'tgs_shop_ignore_notice' ] ) {
+	            add_user_meta( $current_user_id, 'tgs_shop_ignore_notice', 'true', true);
 	    	}				
-			if ( isset($_GET['wcv_pl_ignore_notice']) && '0' == $_GET['wcv_pl_ignore_notice'] ) {
-			 	add_user_meta( $current_user_id, 'wcv_pl_ignore_notice', 'true' , true);
+			if ( isset($_GET['tgs_pl_ignore_notice']) && '0' == $_GET['tgs_pl_ignore_notice'] ) {
+			 	add_user_meta( $current_user_id, 'tgs_pl_ignore_notice', 'true' , true);
 			}
 		}
 
@@ -374,6 +374,6 @@ if ( wcv_is_woocommerce_activated() ) {
 	}
 
 
-	$wc_vendors = new WC_Vendors;
+	$tgs_vendors = new TGS_Vendors;
 
 }
